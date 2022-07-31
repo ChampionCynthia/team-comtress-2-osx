6452cefac5bbaab1f170f1d958f69f7a0616caa9
ChampionCynthia <maitre.cynthia@caramail.com>
2022-07-31 13:24:01 +0200

    build_osx.sh now builds directly from Terminal

--- build_osx.sh
+++ build_osx.sh
@@ -19,14 +19,22 @@ export CXX="$(pwd)/devtools/bin/osx32/ccache clang++"
 if [[ ! -f "thirdparty/protobuf-2.6.1/src/.libs/libprotobuf.a" ]]; then
 	pushd .
 	cd "thirdparty/protobuf-2.6.1/"
-	chmod u+x configure
-	./configure "CFLAGS=-m32 -Wno-reserved-user-defined-literal -D_GLIBCXX_USE_CXX11_ABI=0" \
-		"CXXFLAGS=-m32 -Wno-reserved-user-defined-literal -D_GLIBCXX_USE_CXX11_ABI=0" \
-		"LDFLAGS=-m32"
-	chmod u+x install-sh
+	./configure "CFLAGS=-m32 -mmacosx-version-min=10.7 -Wno-reserved-user-defined-literal -D_GLIBCXX_USE_CXX11_ABI=0" \
+		"CXXFLAGS=-m32 -mmacosx-version-min=10.7 -Wno-reserved-user-defined-literal -D_GLIBCXX_USE_CXX11_ABI=0" \
+		"LDFLAGS=-m32" \
+		"SDKROOT=\"\""
 	make "-j$CORES"
 	popd
 fi
+
+# if [[ ! -f "lib/common/osx32/libcryptopp.a" ]]; then
+#	pushd .
+#	cd "external/crypto++-5.6.3/"
+#	make "-j$CORES" "CFLAGS=-m32 -mmacosx-version-min=10.7 -stdlib=libc++" \
+#		"CXXFLAGS=-m32 -mmacosx-version-min=10.7 -stdlib=libc++" \
+#		"SDKROOT=\"\""
+#	popd
+# fi
 
 if [[ ! -f "./devtools/bin/vpc_osx" ]]; then
 	pushd .
@@ -40,5 +48,5 @@ fi
 devtools/bin/vpc_osx /define:WORKSHOP_IMPORT_DISABLE /define:SIXENSE_DISABLE /define:NO_X360_XDK \
 				/define:RAD_TELEMETRY_DISABLED /define:DISABLE_ETW /retail /tf ${VPC_FLAGS} +game /mksln games
 
-echo "Open games.xcproj in Xcode 9.4.1."
+xcodebuild -workspace "$(pwd)/games.xcodeproj/project.xcworkspace" -scheme All -configuration Debug
 
